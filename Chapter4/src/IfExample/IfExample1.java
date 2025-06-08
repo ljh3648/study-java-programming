b8029cfb4619705466c6a909ff6e9aed595da8b1
Lee Jihun <ljh09060@gmail.com>
2025-06-08 23:23:37 +0900

    feat: Add new solutions

---
+++ Chapter4/src/IfExample/IfExample1.java
@@ -0,0 +1,15 @@
+package IfExample;
+
+import java.io.IOException;
+
+public class IfExample1 {
+    public static void main(String[] args) throws IOException {
+        int age = 2;
+        if (age >= 0) {
+            System.out.println("학교에 다닙니다.");
+        }
+        else {
+            System.out.println("학교에 다니지 않습니다.");
+        }
+    }
+}
